3ee76a9d4c3962807daacf96e6765e4ccd105244
Victor Jhepherson <victor_jhepherson@hotmail.com>
2021-02-01 20:53:57 -0300

    Login - ajustes

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const LoginController = require('../controllers/LoginController');
 
 router.post('/', LoginController.login);
-router.post('/refresh', LoginController.refresh);
+router.post('/refresh', login, LoginController.refresh);
 
 module.exports = router;
